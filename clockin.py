bebff0ebee1c8d14a59535c3df8a93d7a15ea3e3
tadaki <tadaki.asechi@gmail.com>
2023-07-05 20:12:24 +0900

    add another button and make routing.py.

--- clockin.py
+++ clockin.py
@@ -2,11 +2,22 @@
 
 import tkinter as tk
 import test_logic as logi
+import routing as route
 
+# Tkクラスを作成
 root = tk.Tk()
-root.geometry("250x100")
+root.geometry("300x200")
 
-run_button = tk.Button(root, text="Run", command=logi.hello)
-run_button.place(x=90,y=30)
+# タイトルを作成
+root.title("clockin")
 
+# 出勤ボタンを作成
+clock_in = tk.Button(root, text="出勤", command=route.clock_in)
+clock_in.place(x=40, y=30)
+
+# 退勤ボタンを作成
+clock_out = tk.Button(root, text="退勤", command=route.clock_out)
+clock_out.place(x=160, y=30)
+
+# 表示
 root.mainloop()
---
+++ routing.py
@@ -0,0 +1,6 @@
+
+def clock_in():
+    print("Hello!")
+ 
+def clock_out():
+    print("Goodbye!")   
